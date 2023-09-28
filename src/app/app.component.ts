293a7df49fe8ac844084f3bcbfcdb13eb8b6f590
Elodie <elodie.dargot@gmail.com>
2023-09-28 20:07:25 +0200

    exercice terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NasaService } from './nasa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nasaApi';
+export class AppComponent implements OnInit {
+
+  public imgOfTheDay: any = "";
+
+  constructor(private nasaService: NasaService){}
+
+  ngOnInit(): void {
+    this.nasaService.getImageOfTheDay().subscribe(image => {
+      this.imgOfTheDay = image.url
+    })
+  }
 }
